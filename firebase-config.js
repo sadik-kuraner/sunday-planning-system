0a805bd88a454666c8c076bc55b75dcb0c7d37c6
Sadik Kuraner <s_kuraner@hotmail.com>
2025-12-09 14:33:23 +0100

    Full update

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUyxNS8S490CPY6MSmVSKYZAkR9BxsXig",
+  authDomain: "sunday-planning-system.firebaseapp.com",
+  projectId: "sunday-planning-system",
+  storageBucket: "sunday-planning-system.firebasestorage.app",
+  messagingSenderId: "986157390486",
+  appId: "1:986157390486:web:5970b2c20e307b140880b7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
